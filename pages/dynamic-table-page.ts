f67a9ae3ec6c95c942e7fe69d89ce815b7fb1c3b
belidor <idietoday@ya.ru>
2022-11-06 14:08:12 +0100

    idk why page objects doesn't work

--- pages/dynamic-table-page.ts
+++ pages/dynamic-table-page.ts
@@ -4,12 +4,12 @@ export class DynamicTablePage {
   page: Page;
   tableHeader: Locator;
 
-  costructor(page : Page) {
+  costructor(page: Page) {
     this.page = page;
     this.tableHeader = page.locator('body > div > table > thead > tr > th:nth-child(1)');
   }
 
-  async openApp() {
+  async openApp(): Promise<void> {
     await this.page.goto('https://qaplayground.dev/apps/dynamic-table/');
     await expect(this.tableHeader).toBeVisible();
   }
--- tests/dynamic-table.spec.ts
+++ tests/dynamic-table.spec.ts
@@ -4,9 +4,10 @@ import { DynamicTablePage } from '../pages/dynamic-table-page';
 test.describe("Dynamic table app tests", () => {
 
     test.beforeEach(async ({ page }) => {
-      const dynamicTablePage = new DynamicTablePage();
+      const tableHeader = page.locator('body > div > table > thead > tr > th:nth-child(1)');
 
-      await dynamicTablePage.openApp();
+      await page.goto('https://qaplayground.dev/apps/dynamic-table/');
+      await expect(tableHeader).toBeVisible();
     });
 
     test("Should verify Spider-Man's real name", async ({ page }) => {
